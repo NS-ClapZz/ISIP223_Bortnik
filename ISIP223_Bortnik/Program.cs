4fd4b50958832575e1bbc1d7b32b380c952de10b
Иван Бортник <96baks96@gmail.com>
2025-10-08 13:24:15 +0300

    Классы предметов: оружие, броня, зелья

--- ISIP223_Bortnik/Program.cs
+++ ISIP223_Bortnik/Program.cs
@@ -32,3 +32,62 @@ namespace RoguelikeGame
 
         public abstract string GetStats();
     }
+
+    // Класс оружия
+    public class Weapon : Item
+    {
+        public int Attack { get; private set; }
+
+        public Weapon(string name, int attack) : base(name)
+        {
+            Attack = attack;
+        }
+
+        public override void Use(Player player)
+        {
+            player.EquipWeapon(this);
+        }
+
+        public override string GetStats()
+        {
+            return $"{Name} (Атака: {Attack})";
+        }
+    }
+
+    // Класс доспехов
+    public class Armor : Item
+    {
+        public int Defense { get; private set; }
+
+        public Armor(string name, int defense) : base(name)
+        {
+            Defense = defense;
+        }
+
+        public override void Use(Player player)
+        {
+            player.EquipArmor(this);
+        }
+
+        public override string GetStats()
+        {
+            return $"{Name} (Защита: {Defense})";
+        }
+    }
+
+    // Класс лечебного зелья
+    public class Potion : Item
+    {
+        public Potion() : base("Лечебное зелье") { }
+
+        public override void Use(Player player)
+        {
+            player.Heal();
+            Console.WriteLine("Вы выпили лечебное зелье и полностью восстановили здоровье!");
+        }
+
+        public override string GetStats()
+        {
+            return $"{Name} (Полное восстановление здоровья)";
+        }
+    }
